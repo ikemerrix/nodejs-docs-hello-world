d861cef85d6809ed1fa0d589c25f3a96de87021d
Isaac Morrison <ikemerrix@outlook.com>
2020-03-05 15:43:42 -0800

    added folks

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
-    response.end("Hello Githubfolksof the day of newyork");});
+    response.end("Hello Git folks of the day of newyork");});
 
 const port = process.env.PORT || 1337;
 server.listen(port);
